1279d3e5d4ecaf86dca2ab7570e7227e591895a0
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-03-23 17:58:53 +0100

    title

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,22 +1,7 @@
 module.exports = {
     index: (req, res) => {
-        // rendering from the default folder
+        // rendering from the admin folder
         res.render('admin/index')
     },
-    // loginGet: (req, res) => {
-    //     // rendering from the default folder login file
-    //     res.render('default/login')
-    // },
-    // loginPost: (req, res) => {
-    //     // rendering from the default folder 
-    //     res.send('submited a form')
-    // },
-    // registerGet: (req, res) => {
-    //     // rendering from the default folder register file
-    //     res.render('default/register')
 
-    // },
-    // registerPost: (req, res) => {
-    //     res.send('submited a register form')
-    // }
 }
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,13 +4,4 @@ const adminController = require('../controllers/adminController')
 router.route('/')
     .get(adminController.index)
     
-
-// router.route('/login')
-//     .get(adminController.loginGet)
-//     .post(adminController.loginPost)
-
-// router.route('/register')
-//     .get(adminController.registerGet)
-//     .post(adminController.registerPost)
-
 module.exports = router
