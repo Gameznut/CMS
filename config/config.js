8f8823a926cb8594b89f81d9cae3b2894d5d0ab2
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-03-23 16:10:12 +0100

    getting there

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODBURI : 'mongodb://localhost:27017/CMS'
+}
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const {MONGODBURI} = require('./config/config')
 const PORT = process.env.PORT || 3000
+const path = require('path');
+const {create} = require('express-handlebars');
 const app = express()
 
-app.use(express.json())
+// conneting to mogodb
+mongoose.connect(MONGODBURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(()=>{
+            console.log('connected to db');
+        })
+        .catch((error)=>{
+            console.log(error.message);
+        })
 
-app.get('/', (req, res) => {
-    res.send('hello worlds')
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// set up views engine
+app.set('view engine', 'handlebars')
+app.engine('handlebars', create({defaultLayout: 'default'}).engine);
+
+
+app.use('/', (req, res) => {
+    // res.send('hello world')
+    res.render('default/index')
 })
 
 app.listen(PORT, ()=>{
