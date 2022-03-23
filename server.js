37248a86f0adff5f1b0ff9b9717ee8e24639ab0c
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-03-23 14:15:06 +0100

    send

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
 app.listen(PORT, ()=>{
     console.log(`The ${PORT} is active`);
 })
