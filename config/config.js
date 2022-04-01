b23b68d4d30f8b8eadb333d4762e1886020c040f
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-04-01 17:56:42 +0100

    db

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
-    MONGODBURI : process.env.MONGODBURI || 'mongodb://localhost:27017/CMS',
+    MONGODBURI : process.env.MONGODBURI /* || 'mongodb://localhost:27017/CMS' */,
     PORT: process.env.PORT || 3001,
     globalVariable: (req, res, next) => {
         res.locals.success_message = req.flash('success-message');
